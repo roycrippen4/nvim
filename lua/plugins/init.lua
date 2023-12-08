466bfce35b971d4a95959e10884f159da818f5c2
roy.crippen4 <roy.crippen4@archarithms.com>
2023-12-08 16:34:23 -0500

    fixed nvim tree icons

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -71,8 +71,87 @@ local default_plugins = {
       return { override = require 'nvchad.icons.devicons' }
     end,
     config = function(_, opts)
+      local devicon = require 'nvim-web-devicons'
       dofile(vim.g.base46_cache .. 'devicons')
-      require('nvim-web-devicons').setup(opts)
+      devicon.setup(opts)
+      -- 󰎙󰬝󰴜󰫽󱂌
+      devicon.set_icon {
+        ['vite.config.ts'] = {
+          icon = '󰹭',
+          color = '#7c0e8a',
+          name = 'vite.config.ts',
+        },
+        ['package-lock.json'] = {
+          icon = '',
+          color = '#7a0d21',
+          name = 'package-lock.json',
+        },
+        ['package.json'] = {
+          icon = '',
+          color = '#e8274b',
+          name = 'package.json',
+        },
+        ['.nvmrc'] = {
+          icon = '󰎙',
+          color = '#005500',
+          name = '.nvmrc',
+        },
+        ['.npmrc'] = {
+          icon = '󰎙',
+          color = '#007700',
+          name = '.npmrc',
+        },
+        ['tsconfig.test.json'] = {
+          icon = '󰙨',
+          color = '#519aba',
+          name = 'tsconfig.json',
+        },
+        ['tsconfig.json'] = {
+          icon = '',
+          color = '#519aba',
+          name = 'tsconfig.json',
+        },
+        ['tsconfig.node.json'] = {
+          icon = '',
+          color = '#519aba',
+          name = 'tsconfig.node.json',
+        },
+        ['.eslintignore'] = {
+          icon = '',
+          color = '#1a455f',
+          name = '.eslintignore',
+        },
+        ['.eslintrc'] = {
+          icon = '',
+          color = '#2c739e',
+          name = 'Eslintrc',
+        },
+        ['.eslintrc.cjs'] = {
+          icon = '',
+          color = '#2c739e',
+          name = 'Eslintrc',
+        },
+        ['.eslintrc.json'] = {
+          icon = '',
+          color = '#2c739e',
+          name = 'Eslintrc',
+        },
+        ['.eslintrc.yaml'] = {
+          icon = '',
+          color = '#2c739e',
+          name = 'Eslintrc',
+        },
+        ['.prettierrc'] = {
+          icon = '',
+          color = '#b11a7c',
+          name = '.prettierrc',
+        },
+        ['.prettierignore'] = {
+          icon = '',
+          color = '#580d3e',
+          name = '.prettierignore',
+        },
+      }
     end,
   },
 

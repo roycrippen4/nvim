2c6490b536cbc95aa666c5269dceee2567b55c44
roy.crippen4 <roy.crippen4@archarithms.com>
2023-12-11 12:04:45 -0500

    changed stylua to use 4 space indentation

--- ftplugin/javascript.lua
+++ ftplugin/javascript.lua
@@ -1,9 +1,9 @@
 vim.keymap.set('n', '<Leader>js', function()
-  local path = vim.api.nvim_buf_get_name(0)
-  local filetype = string.sub(path, -2)
-  if filetype == 'js' then
-    vim.keymap.set('n', '<leader>js', function()
-      require('nvterm.terminal').send('node ' .. path, 'float')
-    end, { desc = 'Run Javascript file' })
-  end
+    local path = vim.api.nvim_buf_get_name(0)
+    local filetype = string.sub(path, -2)
+    if filetype == 'js' then
+        vim.keymap.set('n', '<leader>js', function()
+            require('nvterm.terminal').send('node ' .. path, 'float')
+        end, { desc = 'Run Javascript file' })
+    end
 end)

146d8fe839e9057cd2f6f04050f3eee1a9b7c4bb
roy.crippen4 <roy.crippen4@archarithms.com>
2023-12-15 14:06:11 -0500

    updated devicons

--- lua/plugins/configs/devicon.lua
+++ lua/plugins/configs/devicon.lua
@@ -1,49 +1,19 @@
-local devicons = require 'nvim-web-devicons'
-devicons.set_icon {
+local devicons = require('nvim-web-devicons')
+devicons.set_icon({
   ['bun.lockb'] = {
     icon = '󰯮',
     color = '#EEEE00',
     name = 'bun.lockb',
   },
-  ['vite.config.ts'] = {
-    icon = '󰹭',
-    color = '#7c0e8a',
-    name = 'vite.config.ts',
+  ['.env'] = {
+    icon = '',
+    color = 'gray',
+    name = '.env',
   },
-  ['package-lock.json'] = {
-    icon = '',
-    color = '#7a0d21',
-    name = 'package-lock.json',
-  },
-  ['package.json'] = {
-    icon = '',
-    color = '#e8274b',
-    name = 'package.json',
-  },
-  ['.nvmrc'] = {
-    icon = '󰎙',
-    color = '#005500',
-    name = '.nvmrc',
-  },
-  ['.npmrc'] = {
-    icon = '󰎙',
-    color = '#007700',
-    name = '.npmrc',
-  },
-  ['tsconfig.test.json'] = {
-    icon = '󰙨',
-    color = '#519aba',
-    name = 'tsconfig.json',
-  },
-  ['tsconfig.json'] = {
-    icon = '',
-    color = '#519aba',
-    name = 'tsconfig.json',
-  },
-  ['tsconfig.node.json'] = {
-    icon = '',
-    color = '#519aba',
-    name = 'tsconfig.node.json',
+  ['.development.env'] = {
+    icon = '',
+    color = 'gray',
+    name = '.development.env',
   },
   ['.eslintignore'] = {
     icon = '',
@@ -51,6 +21,11 @@ devicons.set_icon {
     name = '.eslintignore',
   },
   ['.eslintrc'] = {
+    icon = '',
+    color = '#2c739e',
+    name = 'Eslintrc',
+  },
+  ['.eslintrc.js'] = {
     icon = '',
     color = '#2c739e',
     name = 'Eslintrc',
@@ -70,6 +45,36 @@ devicons.set_icon {
     color = '#2c739e',
     name = 'Eslintrc',
   },
+  ['http'] = {
+    icon = '󰖟',
+    color = '#519aba',
+    name = 'Http',
+  },
+  ['makefile'] = {
+    icon = '',
+    color = '#f1502f',
+    name = 'Makefile',
+  },
+  ['.nvmrc'] = {
+    icon = '󰎙',
+    color = '#005500',
+    name = '.nvmrc',
+  },
+  ['.npmrc'] = {
+    icon = '󰎙',
+    color = '#007700',
+    name = '.npmrc',
+  },
+  ['package.json'] = {
+    icon = '',
+    color = '#e8274b',
+    name = 'package.json',
+  },
+  ['package-lock.json'] = {
+    icon = '',
+    color = '#7a0d21',
+    name = 'package-lock.json',
+  },
   ['.prettierrc'] = {
     icon = '',
     color = '#b11a7c',
@@ -80,4 +85,64 @@ devicons.set_icon {
     color = '#580d3e',
     name = '.prettierignore',
   },
-}
+  ['tags'] = {
+    icon = '',
+    color = '#bbbbbb',
+    name = 'Tags',
+  },
+  ['tailwind.config.js'] = {
+    icon = '󱏿',
+    color = '#4DB6AC',
+    name = 'tailwind',
+  },
+  ['tailwind.config.cjs'] = {
+    icon = '󱏿',
+    color = '#4DB6AC',
+    name = 'tailwind',
+  },
+  ['tailwind.config.ts'] = {
+    icon = '󱏿',
+    color = '#4DB6AC',
+    name = 'tailwind',
+  },
+  ['tailwind.config.cts'] = {
+    icon = '󱏿',
+    color = '#4DB6AC',
+    name = 'tailwind',
+  },
+  ['tsconfig.json'] = {
+    icon = '',
+    color = '#519aba',
+    name = 'tsconfig.json',
+  },
+  ['tsconfig.node.json'] = {
+    icon = '',
+    color = '#519aba',
+    name = 'tsconfig.node.json',
+  },
+  ['tsconfig.test.json'] = {
+    icon = '󰙨',
+    color = '#519aba',
+    name = 'tsconfig.json',
+  },
+  ['vite.config.cts'] = {
+    icon = '󰹭',
+    color = '#7c0e8a',
+    name = 'vite.config.ts',
+  },
+  ['vite.config.cjs'] = {
+    icon = '󰹭',
+    color = '#7c0e8a',
+    name = 'vite.config.ts',
+  },
+  ['vite.config.js'] = {
+    icon = '󰹭',
+    color = '#7c0e8a',
+    name = 'vite.config.ts',
+  },
+  ['vite.config.ts'] = {
+    icon = '󰹭',
+    color = '#7c0e8a',
+    name = 'vite.config.ts',
+  },
+})
